a214da7be4abed0813245a2f784e323e0bc741e4
haovipaws <darkclawsthenightwing@gmail.com>
2020-11-10 20:58:45 -0800

    forgot what this commit was

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/bin/python3
 
+import os
 from flask import Flask
+from flask import send_from_directory
+
 app = Flask(__name__)
 
 
@@ -8,8 +11,9 @@ app = Flask(__name__)
 @app.route('/')
 def main_page():
     # Smart code stuff here
-    return 'good morning'
+    return '<link rel="shortcut icon" href="{{ url_for(static, filename=favicon.ico) }}">'
 
 @app.route('/favicon.ico')
 def favicon():
-    return 'a'
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
